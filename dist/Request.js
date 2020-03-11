dc64f1fe10723060aba886786dd7a4ea2c55c57d
Pi-Bouf <pierre.bouffier05@gmail.com>
2020-03-11 21:49:23 +0100

    Upgrade tsconfig

--- dist/Request.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Request = (function () {
-    function Request() {
-        console.log("ZOB");
-    }
-    return Request;
-}());
-exports.Request = Request;
--- src/Request.ts
+++ src/Request.ts
@@ -1,9 +1,10 @@
 export class Request<T> {
-    private _data: T;
+    private _data: T | undefined;
     private _loaded: boolean;
     private _listeners: Array<() => void>;
 
     constructor() {
+        this._data = undefined;
         this._loaded = false;
         this._listeners = [];
     }
